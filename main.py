2b1b0093d276cd833b51390d98b57c56b0918a6c
Theriley106 <christopherlambert106@gmail.com>
2017-08-17 16:25:03 -0400

    Made it return percentage rather than numerical value

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def calcDiff(stock):
 	get_value_url = 'http://finance.google.com/finance/info?client=ig&q=' + stock
 	value = subprocess.Popen(['curl', '-s', get_value_url], stdout=subprocess.PIPE).communicate()[0]
 	j = json.loads(value[5:len(value)-2])
-	return float(j['l']) - first
+	return float((float(j['l']) - first) / float(first))
 
 
 if __name__ == "__main__":
