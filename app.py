ff1e2c26921b3d46e0efd220d3e8dada05e3c25f
Theriley106 <christopherlambert106@gmail.com>
2017-10-17 13:25:19 -0400

    Added gitignore

--- app.py
+++ app.py
@@ -3,41 +3,51 @@ from datetime import datetime
 import csv
 import random
 import os
+import datetime
+LISTOFSTOCKS = []
+with open('NYSE.csv', 'rb') as f:
+    reader = csv.reader(f)
+    listStock = list(reader)
 
-LISTOFSTOCKS = ["TSLA", "AAPL", "GOOG"]
+for e in listStock:
+	if len(str(e[0])) > 2:
+		LISTOFSTOCKS.append(e[0])
 
 app = Flask(__name__, static_url_path='/static')
 
+@app.route('/<stock>', methods=['POST', "GET"])
+def mainStock(stock):
+	stockTicker = stock.lower()
+	return render_template('index.html', stock=stock, stockTicker=stockTicker, currentTime=datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
+	
 @app.route('/', methods=['POST', "GET"])
-def song():
-	return render_template('index.html')
-
-@app.route('/something.svg')
-def graph_something():
-    bar_chart = pygal.Bar()
-    bar_chart.add('Fibonacci', [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
-    return bar_chart.render_response()
+def genStock():
+	stock = random.choice(LISTOFSTOCKS)
+	
+	return redirect(url_for('mainStock', stock=stock))
 
 @app.route('/calc/', methods=['POST'])
 def genTrade(stock=False, position=False):
-	for values in list(request.form.items()):
-		if str(values[1]).upper() in LISTOFSTOCKS:
-			stock = str(values[1]).upper()
-		if "Short" in str(values):
-			position = "Short"
-		if "Long" in str(values):
-			position = "Long"
-	if stock == False:
-		stock = random.choice(LISTOFSTOCKS)
-	if position == False:
-		position = random.choice(["Long", "Short"])
-	
-	return "It looks like you want to {} {}".format(position, stock)
+	a = []
+	results = list(request.form.items())
+	for e in results:
+		if "Ticker" in str(e):
+			a.append(e)
+	a.append(e)
+	results = a
+	position = list(results)[1][0]
+	stock = list(results)[0][1]
+	for e in results:
+		print e
+	return redirect(url_for('mainStock', stock=stock))
+	#return "It looks like you want to {} {}".format(position, stock)
 
 
 @app.route('/trades/<ticker>', methods=['POST', "GET"])
 def showTrades(ticker):
 	return str(ticker)
 
+
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+	app.run(host='0.0.0.0', port=5000, debug=True)
